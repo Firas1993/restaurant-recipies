b30450b54e65ea576f697484ac635d71e2a671f7
Firas1993 <kebsi.firas@competitoor.com>
2019-10-29 08:24:27 +0100

    setup the project files, react bootstrap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Hello FoodFork</h1>
+    </div>
+  );
+}
+
+export default App;
