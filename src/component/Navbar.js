d3002b22b7528a3cb1477866c0bb0c4eedb8bd29
Firas1993 <kebsi.firas@competitoor.com>
2019-10-30 22:17:24 +0100

    comit navbar and link

---
+++ src/component/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from "../images/logo.svg";
+import {Link} from "react-router-dom";
+const Navbar = () => {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+        </Link>
+        <div className="collapse navbar-collapse show ml-sm-5">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/recipes">Recipes</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
