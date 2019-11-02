6a7f313b89dffee58b77822209cbaa9229d78815
Firas1993 <kebsi.firas@competitoor.com>
2019-11-02 12:11:47 +0100

    fixing css 404

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,12 +1,21 @@
 //whene there is no route
 import React, { Component } from 'react'
-
+import Header from "../component/Header";
+import {Link} from "react-router-dom";
 export default class Default extends Component {
     render() {
         return (
-            <h4>
-               Hello error 404 
-            </h4>
-        )
+          <Header title="404" styleClass="default-hero">
+            <h2 className="text-light text-uppercase">
+              you are in the wrong place
+            </h2>
+            <Link
+              to="/"
+              className="text-uppercase btn btn-secondary btn-lg mt-3"
+            >
+              return Home
+            </Link>
+          </Header>
+        );
     }
 }
