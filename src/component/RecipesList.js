c5292676dfc27e684ac568e6e36d16d53a94da29
Firas1993 <kebsi.firas@competitoor.com>
2019-11-06 13:05:00 +0100

    recipe mapping

--- src/component/RecipesList.js
+++ src/component/RecipesList.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Recipe from "./Recipe";
 export default class RecipesList extends Component {
-    render() {
-        return (
-          <div>
-            <h1>recipes list</h1>
-            <h3><Recipe/></h3>
+  render() {
+    const recipes = this.props.recipes;
+    return (
+      <div>
+        <div className="container py-5">
+          {/** title */}
+          <div className="col-10 mx-auto col-md-6 text-center text-text-uppercase mb-3">
+            <h1 className="text-slanted">
+            recipe list
+            </h1>
           </div>
-        );
-    }
+          {/**end of title */}
+          <div className="row">
+          {
+            recipes.map(recipe =>{
+              return <Recipe   
+              key={recipe.recipe_id}
+              recipe={recipe}
+                     />;})
+          }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
