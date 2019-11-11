c3baba9ea8af61189136a6972095a97f79ae6894
Firas1993 <kebsi.firas@competitoor.com>
2019-11-11 22:56:45 +0100

    preparing the static data for ajax call single recipe page

--- src/component/Recipe.js
+++ src/component/Recipe.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {Link} from "react-router-dom";
 export default class Recipe extends Component {
     render() {
-        const {image_url, source_url, title,publisher} = this.props.recipe;
+        const {image_url, source_url, title,publisher,recipe_id} = this.props.recipe;
         return(
             <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
               <div className="card" style={{height:"100%"}}>
@@ -10,7 +10,17 @@ export default class Recipe extends Component {
                 <div className="card-body text-capitalize">
                   <h6>{title}</h6>
                   <h6 className="text-warning text-slanted">Provided by {publisher} </h6>
-                  <Link></Link>
+                  <Link 
+                  to={`/recipes/${recipe_id}`}
+                  className="btn btn-primary mx-2 text-capitalize"
+                  >details </Link>
+                  <a 
+                  className="btn btn-success mx-2 text-capitalize"
+                  href={source_url}
+                  target="_blank"
+                  rel="noopner noreferrer">recipe source
+                  </a>
+                 
                 </div>
               </div>
             </div>
--- src/pages/SingleRecipes.js
+++ src/pages/SingleRecipes.js
@@ -1,11 +1,96 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
+import { recipeData } from "../data/tempList";
 export default class SingleRecipes extends Component {
+    id = this.props.match.params.id;
+        
+    state = {
+    recipe:recipeData,
+    id:this.id,
+    loading:false
+    }
     render() {
-        return (
-            <h3>
-               recipe id  
-            </h3>
-        )
+        const result = recipeData.find(obj => {
+          return obj.recipe_id === this.id;
+        });
+        const {
+          ingredients,
+          publisher,
+          title,
+          source_url,
+          recipe_id,
+          image_url,
+          publisher_url
+        } = result;
+        
+        if(this.state.loading){
+            return (
+              <div className="container">
+                <div className="row">
+                  <div className="col-10 mx-auto col-md-6 my-3">
+                    <h2 className="text-uppercase text-orange text-center"> loading recipe....</h2>{" "}
+                  </div>
+                </div>
+              </div>
+            );
+        }else{
+          return (
+            <div className="container my-5">
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <Link
+                    to="/recipes"
+                    className="btn btn-warning mb-5 text-capitalize"
+                  >
+                    back to recipes
+                  </Link>
+                  <img
+                    src={image_url}
+                    className="d-block w-100"
+                    style={{ maxHeight: "30rem" }}
+                    alt="recipe"
+                  />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <h6 className="text-uppercase">{title}</h6>
+                  <h6 className="text-warning text-capitalize text-slanted">
+                    provided by {publisher}
+                  </h6>
+                  <a
+                    href={publisher_url}
+                    target="_blank"
+                    rel="noopner noreferrer"
+                    className="btn btn-primary  mt-2 text-capitalize"
+                  >
+                    publisher web page
+                  </a>
+                  <a
+                    href={source_url}
+                    target="_blank"
+                    rel="noopner noreferrer"
+                    className="btn btn-success  mt-2 text-capitalize"
+                  >
+                    recipe web page
+                  </a>
+
+                  <ul className="list-group mt-4">
+                    <h2 className="mt-3 mb-4">Ingredient</h2>
+                    {ingredients.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="list-group-item text-slanted"
+                        >
+                          {item}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          );
+        }
     }
 }
